6ac9770beee4b2682615bab60c9a626a6b83a2cd
Vbalimidi <vb2023@hw.ac.uk>
2024-12-21 15:29:32 +0000

    HTML added

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1> My Todos</h1>
+
+    <div className = "todo-wrapper">
+      <div className = "todo-input">
+        <div className = "input-item">
+        <label> Title</label>
+        <input type="text" placeholder = "What's on the agenda" />
+      </div>
+      <div className = "input-item">
+        <label> Description</label>
+        <input type="text" placeholder = "What do you want to achieve" />
+      </div>
+      <div className = "input-item">
+        <button type='button' className="button"> Add </button>
+      </div>
+      </div>
+
+      <div className = "todo-buttons">
+      <button type='button' className="btns"> Todo </button>
+      <button type='button' className="btns"> Completed </button>
+      </div>
+
+      <div className="todo-list">
+        <div className="todo-lit-item">
+          <h3>Task 1</h3>
+          <p>Description</p>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
